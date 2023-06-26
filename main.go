c48fe470f27dc6aeb355cf77171664ee9fcf1863
Mahmood <mahmoodrm@gmail.com>
2023-06-26 09:31:56 +0330

    bug fix

--- main.go
+++ main.go
@@ -61,7 +61,7 @@ func main() {
 
 func greetUsers() {
 	fmt.Printf("Welcome to %v booking application\n", conferenceName)
-	fmt.Printf("We have total of %v tickets and %v are still available.\n", conferenceTickets, remainingTickets)
+	fmt.Printf("We have total of % tickets and %v are still available.\n", conferenceTickets, remainingTickets)
 	fmt.Println("Get your tickets here to attend")
 }
 
